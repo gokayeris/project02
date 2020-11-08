b9953694605fd663ccf3e7efda20bee6042b037c
Gokay Eris <gokayeris@gmail.com>
2020-11-08 13:09:22 -0300

    second commit from local master

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -3,7 +3,8 @@ package firstPackage;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		System.out.println("First commit from local master");
+		System.out.println("First commit from local master branch");
+		System.out.println("Second commit from local master");
 
 	}
 
